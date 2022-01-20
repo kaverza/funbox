8d0f1c2438a285f723e02e2e8aa5781304743eaf
Артем Каверза <temamail1@yandex.ru>
2022-01-20 22:08:05 +0300

    final

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,6 +1,7 @@
+import {useCallback, memo} from "react";
 import styles from './styles.module.scss';
 import cn from 'classnames';
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import Hint from "../hint";
 
 const Card = ({ isSelected, isDisabled, data = {} }) => {
@@ -14,14 +15,27 @@ const Card = ({ isSelected, isDisabled, data = {} }) => {
   } = data;
 
   const [selected, setSelected] = useState(isSelected);
+  const [isMouseLeave, setIsMouseLeave] = useState(isSelected);
+  const [isHover, setIsHover] = useState(false);
 
-  const onSelected = (selected) => {
+
+  console.log('render');
+
+  //console.log({selected, isHover, isMouseLeave});
+  const onSelectedHandler = (selected) => {
     if (isDisabled) {
       return;
     }
 
+    if (!selected) {
+      setIsHover(selected);
+    }
+
     setSelected(selected);
+    setIsMouseLeave(selected);
   }
+  const onMouseEnterHandler = () => isMouseLeave && setIsHover(true);
+  const onMouseLeaveHandler = () => selected && setIsHover(false);
 
   return (
       <div
@@ -32,11 +46,15 @@ const Card = ({ isSelected, isDisabled, data = {} }) => {
             [styles['card__wrap--selected']]: selected,
             [styles['card__wrap--disabled']]: isDisabled
           })}
-          onClick={() => onSelected(!selected)}
+          onClick={() => onSelectedHandler(!selected)}
+          onMouseEnter={onMouseEnterHandler}
+          onMouseLeave={onMouseLeaveHandler}
         >
           <div className={cn(styles['card__content'])}>
-            <div className={cn(styles['card__pre-title'])}>
-              Сказочное заморское яство
+            <div className={cn(styles['card__pre-title'], {
+              [styles['card__pre-title--cl-red']]: isHover
+            })}>
+              {isHover ? <>Котэ не одобряет?</> : <>Сказочное заморское яство</>}
             </div>
             <h2 className={cn(styles['card__title'])}>
               {title}
@@ -44,7 +62,7 @@ const Card = ({ isSelected, isDisabled, data = {} }) => {
             </h2>
             {features.length && (
               <div className={cn(styles['card__features'])}>
-                {features.map(item => (<p dangerouslySetInnerHTML={{__html: item}} />))}
+                {features.map((item, index) => (<p key={index} dangerouslySetInnerHTML={{__html: item}} />))}
               </div>
             )}
             <div className={cn(styles['card__weight'])}>
@@ -63,7 +81,7 @@ const Card = ({ isSelected, isDisabled, data = {} }) => {
         <Hint
           isSelected={selected}
           isDisabled={isDisabled}
-          onSelect={() => onSelected(!selected)}
+          onSelect={() => onSelectedHandler(!selected)}
 
           defaultText="Чего сидишь? Порадуй котэ,"
           selectedText={composition}
@@ -73,4 +91,4 @@ const Card = ({ isSelected, isDisabled, data = {} }) => {
   );
 }
 
-export default Card;
+export default memo(Card);
